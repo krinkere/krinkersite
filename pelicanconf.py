b722f7501b9dba87e8ff4fe1239651394d524498
Krinker <elman.krinker@uspto.gov>
2018-03-06 15:54:18 -0500

    added search and tables

--- pelicanconf.py
+++ pelicanconf.py
@@ -52,9 +52,22 @@ PLUGINS = [
     'liquid_tags.notebook',
     'liquid_tags.include_code',
     'render_math',
-    'pelican-ipynb.markup']
+    'pelican-ipynb.markup',
+    'tipue_search']
 
 I18N_TEMPLATES_LANG = 'en'
 
 IGNORE_FILES = ['.ipynb_checkpoints']
 
+# for Tique Search Plugin
+DIRECT_TEMPLATES = ('index','tags', 'categories', 'authors', 'archives', 'search')
+
+CUSTOM_CSS = 'static/css/custom.css'
+CUSTOM_JS = 'static/js/custom.js'
+
+STATIC_PATHS = [ 'extra' ]
+
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/css/custom.css'},
+    'extra/custom.js': {'path': 'static/js/custom.js'}
+}
