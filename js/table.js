31d46b4136bca93763c069ebfc36f9270a5a4ca8
Krinker <elman.krinker@uspto.gov>
2018-03-07 12:18:36 -0500

    Update documentation

--- js/table.js
+++ js/table.js
@@ -4,13 +4,13 @@
  * Copyright (c) 2016 Ines Pauer, Philipp Busse, Sebastian Hilbig, Erich Kramer, Deniz Sesli
  * Licensed under the MIT license
  */
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_HTML"></script>
 
 
+/*!
   $(document).ready(function () {
     $("article table").addClass("table table-condensed table-bordered table-hover");
   });
-
+ */
 
 /*!
 var tables, i;
@@ -23,13 +23,5 @@ for (i=0;i<tables.length;i++) {
 var tables, i;
 tables = document.getElementsByTagName('table');
 for (i=0;i<tables.length;i++) {
-  tables[i].className = 'table table-bordered table-hover table-striped';
+  tables[i].className = 'table table-bordered table-hover table-striped table-responsive';
 }
-
-/*!
-<script>
-  $(document).ready(function () {
-    $("table").attr("class","table table-condensed table-bordered");
-  });
-</script>
- */
